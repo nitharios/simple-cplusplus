456dc82d24ae1aceb56c5b4c1e1effb9ce90872f
Nathan N <Nitharios@users.noreply.github.com>
2018-03-19 21:15:47 -1000

    basic setup with bugs

--- app.cpp
+++ app.cpp
@@ -1,2 +1,43 @@
 #include <iostream>
 using namespace std;
+
+class Calculator
+{
+public:
+  int Calculate(int, int, char);
+};
+
+int main()
+{
+  int x, y, result;
+  char oper;
+  cout << "Hello! I am a calculator!" << endl;
+  cout << "Please enter num1 operator num2: " << endl;
+  cin >> x >> y >> oper;
+  Calculator c;
+  result = c.Calculate(x, y, oper);
+  cout << "Result is: " << result << endl;
+
+  cin.ignore();
+  cin.get();
+  return 0;
+};
+
+int Calculator::Calculate(int x, int y, char oper)
+{
+  switch (oper)
+  {
+  case "+":
+    return x + y;
+  case "-":
+    return x - y;
+  case "*":
+    return x * y;
+  case "/":
+    if (y != 0)
+      return x / y;
+  default:
+    cin >> x >> oper >> y;
+    Calculate(x, y, oper);
+  };
+}
